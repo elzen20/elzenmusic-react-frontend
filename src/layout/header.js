20093efbab1e98abddfb6645d7b27a34a58d7b32
elzen20 <ceqc.quintero@gmail.com>
2024-04-12 14:22:36 -0600

    Evitrando que se refresque la pagina cada vez que el usuario selecciona una ruta

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -41,7 +42,7 @@ function Header(props) {
                 {items &&
                   items.map((item, key) => (
                     <li key={key}>
-                      <Nav.Link href={item.url}>{item.title}</Nav.Link>
+                      <Link to={item.url}>{item.title}</Link>
                     </li>
                   ))}
                 <Button variant="success">
